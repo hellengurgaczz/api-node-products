1802a93b06320e4c147f1fee3c761ebfbd91af92
hellen <gurgaczzhellen@gmail.com>
2023-03-07 10:17:31 -0300

    add

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const server = express();
+const repository = require('./db');
 
 server.listen(process.env.PORT), () => {
     console.log(`Servidor rodando na porta de conexão ${process.env.PORT}.`)
 };
 
-server.get('/products', (req, res) => {
-    res.send('Products!');
-})
+// Routes
+server.get('/products', repository.getProducts)
+server.get('/products/:id', repository.getProductById)
+server.post('/products', repository.createProduct)
+server.update('/products/:id', repository.updateProduct)
+server.delete('/products/:id', repository.deleteProduct)
---
+++ products/repositories/productRepository.js
@@ -0,0 +1,19 @@
+const getProducts = (req, res) => {
+
+}
+
+const getProductById = (req, res) => {
+    
+}
+
+const createProduct = (req, res) => {
+    
+}
+
+const updateProduct = (req, res) => {
+    
+}
+
+const deleteProduct = (req, res) => {
+    
+}
